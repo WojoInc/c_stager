e9cc4c7eddafed2aacf00b8082754cb5c31fd63f
Thomas Wesolowski <wojoinc@iastate.edu>
2017-07-02 21:29:39 -0500

    Added function to load payload shellcode from file. Fixed formatting.

--- server.c
+++ server.c
@@ -14,6 +14,41 @@
 
 typedef unsigned char byte_t;
 
+/**
+ * Loads payload shellcode from path pointed to by 'path'.
+ * @param path File path to load payload from
+ * @param payload byte array containing loaded shellcode
+ * @return number of bytes read, -1 on failure
+ */
+int load_payload(char *path, byte_t **payload) {
+    FILE *pld = fopen(path, "rb");
+    if (pld == NULL) {
+        fprintf(stderr, "Could not open payload file.");
+        return -1;
+    }
+    /*
+     * Get file size, using int here, as shellcode length
+     * is by design only a few bytes in length. Can be edited later
+     * should the need arise.
+     */
+    int size = 0;
+    fseek(pld, 0, SEEK_END);
+    size = ftell(pld);
+    fseek(pld, 0, SEEK_SET);
+
+    /*
+     * Allocate memory for payload so it is not read directly from disc
+     * when transmitting.
+     * Read payload into memory.
+     */
+    *payload = (byte_t *) malloc(size * sizeof(byte_t));
+    for (int i = 0; i < size; i++) {
+        //TODO determine if endianess matters, but shouldn't as we are reading byte-by-byte
+        fread(*payload + i, 1, 1, pld);
+    }
+    return size;
+}
+
 int main(int argc, char **argv) {
     /*
      * Declare socket file descriptors for socket syscall, and accept syscall.
@@ -25,21 +60,26 @@ int main(int argc, char **argv) {
 
 
     //declare variable to store shellcode and length of shellcode
+    byte_t *payld;
+    int payld_sz;
+
+    payld_sz = load_payload(argv[2], &payld);
+
     //declare byte buffer to store transferred information
     byte_t buf[256];
 
     //declare structs to store address of server and client
     struct sockaddr_in serv_addr, cli_addr;
 
-    if(argc<2){
-        fprintf(stderr,"Please specify port.");
+    if (argc < 2) {
+        fprintf(stderr, "Please specify port.");
         exit(1);
     }
     //create socket in the internet domain using tcp protocol, and port number
     //provided by user
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    if(sockfd<0){
-        fprintf(stderr,"Could not open socket.");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "Could not open socket.");
         exit(2);
     }
     //set all of struct serv_addr to 0
@@ -53,12 +93,12 @@ int main(int argc, char **argv) {
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
     //Bind socket to address
-    listen(sockfd,5);
+    listen(sockfd, 5);
 
     len_cli = sizeof(cli_addr);
-    acceptfd = accept(sockfd,(struct sockaddr*) &cli_addr,(socklen_t *) &len_cli);
+    acceptfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &len_cli);
 
-    bzero(buf,256);
+    bzero(buf, 256);
 
     //TODO add code here to send shellcode size of fixed length of 4 bytes
     //TODO add code to transmit shellcode to connected client.
